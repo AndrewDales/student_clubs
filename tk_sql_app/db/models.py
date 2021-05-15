2a180c2409412a17ae3250011e1ac6cbe5a8f096
Andrew Dales <andrew.dales@highgateschool.org.uk>
2021-05-15 18:45:40 +0100

    Corrected bug - fixed the unique constraint on the person_id and activity_id in the person_activities table

--- tk_sql_app/db/models.py
+++ tk_sql_app/db/models.py
@@ -4,7 +4,7 @@
 
 from sqlalchemy import (
     Column, ForeignKey, ForeignKeyConstraint, Table, UniqueConstraint, event,
-    Boolean, Date, Integer, Text, String
+    Boolean, Date, Integer, Text, String, Unicode
 )
 from sqlalchemy.engine import Engine
 from sqlalchemy.ext.declarative import declarative_base
@@ -18,15 +18,15 @@ person_activities = Table('person_activities',
                           Base.metadata,
                           Column('id', Integer, primary_key=True),
                           Column('activity_id', ForeignKey('activity.id')),
-                          Column('person_id', ForeignKey('person.id'))
+                          Column('person_id', ForeignKey('person.id')),
+                          UniqueConstraint('activity_id', 'person_id')
                           )
-person_activities.__table_args__ = UniqueConstraint('name', 'vehiclemodel_id')
 
 
 # Sets up an Activity table, this references "attendees" via the person_activities table
 class Activity(Base):
     __tablename__ = 'activity'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     name = Column(String, unique=True, nullable=False)
     attendees = relationship("Person",
                              secondary=person_activities,
@@ -40,7 +40,7 @@ class Activity(Base):
 # Sets up a Person table, this references "activities" via the person_activities table
 class Person(Base):
     __tablename__ = 'person'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable=False)
     activities = relationship("Activity",
@@ -50,6 +50,3 @@ class Person(Base):
     # Gives a representation of a Person (for printing out)
     def __repr__(self):
         return f"<Person({self.first_name} {self.last_name})>"
-
-
-
